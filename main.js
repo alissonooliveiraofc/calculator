5dd82a4bc9b6c8fe759ac19cdf2f9c323069900f
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2026-08-11 16:51:16 -0300

    feat: adicionar atributos de dados aos botões e implementar seleção no JavaScript

---
+++ main.js
@@ -0,0 +1,8 @@
+const numberBtn = document.querySelectorAll("[data-number]");
+const operationBtn = document.querySelectorAll(".operator");
+const equalsBtn = document.querySelector("[data-equals]");
+const deleteBtn = document.querySelector("[data-delete]");
+const allClearBtn = document.querySelector("[data-all-clear]");
+
+const previousOperandTextElement = document.querySelector(".previous-operand");
+const currentOperandTextElement = document.querySelector(".current-operand");
